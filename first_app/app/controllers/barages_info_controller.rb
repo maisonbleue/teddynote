c4dbf51249083f31fc191e04315e16ce1888bb1e
maisonbleue <trastour.vincent@gmail.com>
2015-11-02 10:29:19 +0100

    add first_app

---
+++ first_app/app/controllers/barages_info_controller.rb
@@ -0,0 +1,60 @@
+class BaragesInfoController < ApplicationController
+
+	def order_tab
+		@array.each do |k|
+			if k.include? "French"
+				@new_hash["French"] += 2
+			elsif k.include? "TEPitech"
+				@new_hash["TEPitech"] = 1
+			elsif k.include? "SQL"
+				@new_hash["SQL"] = 1
+			elsif k.include? "Conf"
+				@new_hash["Conf"] = 1
+			else
+				@new_hash["Reseaux"] = 1
+			end
+		end
+	end
+
+	def parsejson
+		@data_hash.each do |key, value|
+			if key['modules']
+				value.each do |k, v|
+					if (k['codemodule'].include? "B-PRO") && (k['title'].include? "French") && (k['grade'] != "Echec")
+						@array << k['title']
+					elsif (k['codemodule'].include? "G-EPI") && (k['grade'] == 'Acquis') && (k['title'].include? "Conf")
+						@array << k['title']
+					end
+				end
+			elsif key['notes']
+				value.each do |k, v|
+					if (k['codemodule'].include? "B-BDD") && (k['final_note'] >= 15) && (k['title'].include? "Test SQL")
+						@array << k['title']
+					elsif (k['codemodule'].include? "B-NET") && (k['final_note'] >= 15)
+						@array << k['title']
+					elsif (k['codemodule'].include? "B-ANG") && (k['final_note'] >= 750) && (k['titlemodule'].include? "750")
+						@array << k['title']
+					end
+				end
+			end
+		end
+		order_tab
+	end
+
+	def getjson
+		login= ""
+		cookies = "curl/cookie.txt"
+		url = "https://intra.epitech.eu/user/#{login}/notes/?format=json"
+		result = `curl -b #{cookies} #{url}`
+		@data_hash = JSON.parse(result, opts = {})
+	end
+
+	def index
+		@array = []
+		@new_hash = Hash["Conf" => 0, "TEPitech" => 0, "SQL" => 0, "Reseaux" => 0, "French" => 0]
+
+		getjson
+		parsejson
+	end
+
+end
---
+++ first_app/test/helpers/barages_info_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class BaragesInfoHelperTest < ActionView::TestCase
+end
